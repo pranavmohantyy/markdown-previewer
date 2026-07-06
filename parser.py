d0936e88fce37d789b30615d153fa907abfa0b1f
pranavmohantyy <mohantypranav147@gmail.com>
2026-07-06 14:30:36 -0700

    add link and image parsing

--- parser.py
+++ parser.py
@@ -16,28 +16,11 @@ def parse_bold(markdown):
 def parse_italic(markdown):
     return markdown.replace('*', '<em>', 1).replace('*', '</em>', 1)
 
-def parse_lists(markdown):
-    lines = markdown.split('\n')
-    html = ''
-    ordered = False
-    for line in lines:
-        if line.startswith('- '):
-            if '<ul>' not in html:
-                html += '<ul>\n'
-            html += f'    <li>{line[2:].strip()}</li>\n'
-        elif line.startswith('1. '):
-            if '<ol>' not in html:
-                html += '<ol>\n'
-                ordered = True
-            html += f'    <li>{line[3:].strip()}</li>\n'
-        else:
-            if ordered:
-                html += '</ol>\n'
-                ordered = False
-            if '<ul>' in html:
-                html += '</ul>\n'
-    if ordered:
-        html += '</ol>\n'
-    if '<ul>' in html:
-        html += '</ul>\n'
-    return html
+def parse_links(markdown):
+    import re
+    return re.sub(r'\[(.*?)\]\((.*?)\)', r'<a href="\2">\1</a>', markdown)
+
+def parse_images(markdown):
+    import re
+    return re.sub(r'!\[(.*?)\]\((.*?)\)', r'<img src="\2" alt="\1">', markdown)
+
