87c386333f387de0ac972813b62f946fbeb62f3a
pranavmohantyy <mohantypranav147@gmail.com>
2026-07-06 07:32:26 -0700

    add code block support

--- parser.py
+++ parser.py
@@ -11,13 +11,48 @@ def parse_headers(markdown):
     return html
 
 def parse_bold(markdown):
-    return markdown.replace('**', '<strong>').replace('**', '</strong>')
+    return markdown.replace('**', '<strong>', 1).replace('**', '</strong>', 1)
 
 def parse_italic(markdown):
-    return markdown.replace('*', '<em>').replace('*', '</em>')
+    return markdown.replace('*', '<em>', 1).replace('*', '</em>', 1)
+
+def parse_code(markdown):
+    return markdown.replace('`', '<code>', 1).replace('`', '</code>', 1)
+
+def parse_blockquotes(markdown):
+    lines = markdown.split('\n')
+    html = ''
+    for line in lines:
+        if line.startswith('> '):
+            html += f'<blockquote>{line[2:].strip()}</blockquote>\n'
+        else:
+            html += line + '\n'
+    return html
+
+def parse_code_block(markdown):
+    lines = markdown.split('\n')
+    in_code_block = False
+    html = ''
+    code_buffer = ''
+    for line in lines:
+        if line.startswith('```'):
+            if in_code_block:
+                html += f'<pre><code>{code_buffer}</code></pre>\n'
+                code_buffer = ''
+                in_code_block = False
+            else:
+                in_code_block = True
+        elif in_code_block:
+            code_buffer += line + '\n'
+        else:
+            html += line + '\n'
+    return html
 
 def parse_markdown(markdown):
-    html = parse_headers(markdown)
-    html = parse_bold(html)
-    html = parse_italic(html)
-    return html
+    parsed = parse_headers(markdown)
+    parsed += parse_bold(markdown)
+    parsed += parse_italic(markdown)
+    parsed += parse_code(markdown)
+    parsed += parse_blockquotes(markdown)
+    parsed += parse_code_block(markdown)
+    return parsed
